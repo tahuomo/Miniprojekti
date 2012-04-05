ee0477b3925879313b315292753d5460a61286d0
Eeva Terkki <juuuro@gmail.com>
2012-04-05 18:01:45 +0300

    main

--- Viitejarjestelma/src/main/java/kumiankka/viitejarjestelma/App.java
+++ Viitejarjestelma/src/main/java/kumiankka/viitejarjestelma/App.java
@@ -3,6 +3,10 @@ package kumiankka.viitejarjestelma;
 public class App {
 
     public static void main(String[] args) {
+        KonsoliIO io = new KonsoliIO();
+        Viitepalvelu vp = new Viitepalvelu();
+        TekstiUI ui = new TekstiUI(io, vp);
         
+        ui.run();
     }
 }
