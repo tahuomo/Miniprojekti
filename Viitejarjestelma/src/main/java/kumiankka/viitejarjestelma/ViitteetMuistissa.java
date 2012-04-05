c6d942d4d8ae03b3259b5b8b1afb52ee05fb1a46
Eeva Terkki <juuuro@gmail.com>
2012-04-05 16:31:09 +0300

    Viitteet muistiin tallentava testiluokka

---
+++ Viitejarjestelma/src/main/java/kumiankka/viitejarjestelma/ViitteetMuistissa.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kumiankka.viitejarjestelma;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViitteetMuistissa implements Viitehallinta {
+    List<Artikkeli> viitteet;
+    
+    public viitetallennusStub() {
+        viitteet = new ArrayList<Artikkeli>();
+    }  
+    
+    public void tallennaViite(Artikkeli a) {
+        viitteet.add(a);
+    }
+
+    public List<Artikkeli> listaaViitteet() {
+        return viitteet;
+    }
+    
+}
