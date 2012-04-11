1e12fa9bd388df41a2c30365e286e660bce63b9b
Eeva Terkki <juuuro@gmail.com>
2012-04-11 16:43:39 +0300

    Viitepalvelun junit-testeja

--- Viitejarjestelma/src/test/java/kumiankka/viitejarjestelma/ViitePalveluTest.java
+++ Viitejarjestelma/src/test/java/kumiankka/viitejarjestelma/ViitePalveluTest.java
@@ -1,13 +1,13 @@
-
 package kumiankka.viitejarjestelma;
 
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ViitePalveluTest {
+    private Viitepalvelu viitepalvelu;
     private Viitekirjanpito tallennusStub;
-    
-    
+    private Viite viite;
+
     public ViitePalveluTest() {
     }
 
@@ -18,20 +18,45 @@ public class ViitePalveluTest {
     @AfterClass
     public static void tearDownClass() throws Exception {
     }
-    
+
     @Before
     public void setUp() {
-        tallennusStub = new Viitekirjanpito();
+        this.tallennusStub = new Viitekirjanpito();
+        this.viitepalvelu = new Viitepalvelu(this.tallennusStub);
+        
+        this.viitepalvelu.teeViite("artikkeli");
+        this.viitepalvelu.lisaaViitteenYleisetTiedot("Otsikko", 2012);
+        this.viite = this.viitepalvelu.getViite();
+                
+
     }
-    
+
     @After
     public void tearDown() {
     }
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-     @Test
-     public void hello() {
-         assertTrue(true);
-     }
+
+    @Test
+    public void konstruktoriLuoOlion() {
+        assertNotNull(this.viitepalvelu);
+    }
+    
+    @Test
+    public void teeViiteLuoOlion() {
+        assertNotNull(this.viitepalvelu.getViite());
+    }
+    
+    @Test
+    public void teeViiteLuoOikeantyyppisenViitteen() {
+        assertEquals(this.viite.getTyyppi(), "artikkeli");
+    }
+    
+    @Test 
+    public void viitteenOtsikkoLisataanOikein() {
+        assertEquals(this.viite.getOtsikko(), "Otsikko");
+    }
+    
+    @Test
+    public void viitteenVuosiLisataanOikein() {
+        assertTrue(this.viite.getVuosi() == 2012);
+    }
 }
