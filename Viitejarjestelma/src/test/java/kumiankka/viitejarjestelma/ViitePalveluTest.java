91e4c2803bba625aac5e908b713a9b2cf66c4f54
Tero Huomo <tero.huomo@helsinki.fi>
2012-04-11 16:14:43 +0300

    ViitePalveluTest testiluokka

--- Viitejarjestelma/src/test/java/kumiankka/viitejarjestelma/ViitePalveluTest.java
+++ Viitejarjestelma/src/test/java/kumiankka/viitejarjestelma/ViitePalveluTest.java
@@ -30,6 +30,8 @@ public class ViitePalveluTest {
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
-//     @Test
-//     public void () {}
+     @Test
+     public void hello() {
+         assertTrue(true);
+     }
 }
