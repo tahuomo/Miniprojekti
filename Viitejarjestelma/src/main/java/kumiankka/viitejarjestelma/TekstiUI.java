66956b7f418ec8b0cb8f187edab2b878849fa21f
Tero Huomo <tero.huomo@helsinki.fi>
2012-04-11 16:57:35 +0300

    Tekstikäyttöliittymään lisäys

--- Viitejarjestelma/src/main/java/kumiankka/viitejarjestelma/TekstiUI.java
+++ Viitejarjestelma/src/main/java/kumiankka/viitejarjestelma/TekstiUI.java
@@ -1,5 +1,6 @@
 package kumiankka.viitejarjestelma;
 
+
 public class TekstiUI {
     private IO io;
     private Viitepalvelu vp;
@@ -19,9 +20,8 @@ public class TekstiUI {
                 io.tulosta("Ohjelma lopetetaan.");
                 break;
             }
-
             if (komento.equals("uusi")) {
-                //kysytään tiedot ja lisätään viite palveluun
+                lisaaViite();
             } else if (komento.equals("listaa")) {
                 listaaViitteet();
             } else if (komento.equals("auta")) {
@@ -30,9 +30,48 @@ public class TekstiUI {
 
         }
     }
+    
+    private void lisaaViite(){
+        String tyyppi = io.lueRivi("Kerro viitetyyppi:");
+        vp.teeViite(tyyppi);
+        kysyViitteenTiedot();
+        kysyArtikkelinTiedot();
+        kysyKirjoittajat();
+        vp.tallennaViite();
+        io.tulosta("Uusi viite lisättiin!");
+  
+        
+  }
 
+    private void kysyViitteenTiedot() {
+        String otsikko = io.lueRivi("Otsikko:");
+        int vuosi = io.lueLuku("Julkaisuvuosi:");
+        vp.lisaaViitteenYleisetTiedot(otsikko, vuosi);  
+    }
+
+    private void kysyArtikkelinTiedot() {
+        String lehdenNimi = io.lueRivi("Lehden nimi:");
+        int lehdenNumero = io.lueLuku("Lehden numero:");
+        int aloitusSivu = io.lueLuku("Ensimmäinen sivu:");
+        int lopetusSivu = io.lueLuku("Viimeinen sivu:");
+        String julkaisija = io.lueRivi("Julkaisija:");
+        
+        vp.lisaaArtikkelinTiedot(lehdenNimi, lehdenNumero, aloitusSivu, aloitusSivu, julkaisija);
+    }
+
+    private void kysyKirjoittajat() {
+        io.tulosta("Anna kirjoittajat, tai anna tyhjä lopettaaksesi");
+        while (true){
+            String etunimi = io.lueRivi("Etunimi:");
+            if (etunimi.isEmpty()) break;
+            String sukunimi = io.lueRivi("Sukunimi:");
+            if (sukunimi.isEmpty()) break;
+            vp.lisaaKirjoittaja(etunimi, sukunimi);
+        }
+    }
+    
     private void kerroKomennot() {
-        String komennot = "\tauta\t- näyttä komennot\n"
+        String komennot = "\tauta\t- näyttää komennot\n"
                 + "\tuusi\t- lisää uusi viite\n"
                 + "\tlistaa\t- listaa viitteet\n"
                 + "\tlopeta\t- lopeta ohjelma";
@@ -40,6 +79,8 @@ public class TekstiUI {
     }
     
     private void listaaViitteet(){
-        io.tulosta(vp.listaaViitteet());
+        String viitteet = vp.listaaViitteet();
+        if (viitteet.isEmpty()) io.tulosta("Ei vielä lisättyjä viitteitä");
+        else io.tulosta(viitteet);
     }
 }
