af6d47cbdeb3380c969e661a5c7de3953da0bd91
Eeva Terkki <juuuro@gmail.com>
2012-04-11 18:16:07 +0300

    Viitetietokanta luotu

---
+++ Viitejarjestelma/src/main/java/kumiankka/viitejarjestelma/Viitetietokanta.java
@@ -0,0 +1,32 @@
+package kumiankka.viitejarjestelma;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Viitetietokanta implements Viitehallinta {
+    private EntityManagerFactory emf = null;
+
+    public Viitetietokanta() {
+        emf = Persistence.createEntityManagerFactory("viitteet");
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    @Override
+    public void tallennaViite(Viite viite) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        em.merge(viite);
+        em.getTransaction().commit();
+    }
+
+    @Override
+    public List<Viite> listaaViitteet() {
+        EntityManager em = getEntityManager();
+        return em.createQuery("SELECT v FROM Viite v").getResultList();
+    }
+}
