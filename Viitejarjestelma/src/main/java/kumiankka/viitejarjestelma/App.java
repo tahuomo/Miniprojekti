4afb6d1b1c7ea6d37fc261eb0bb402fed93f27f9
Tero Huomo <tero.huomo@helsinki.fi>
2012-04-11 17:25:59 +0300

    tietokannan käynnistys Mainiin

--- Viitejarjestelma/src/main/java/kumiankka/viitejarjestelma/App.java
+++ Viitejarjestelma/src/main/java/kumiankka/viitejarjestelma/App.java
@@ -1,13 +1,35 @@
 package kumiankka.viitejarjestelma;
 
+import org.hsqldb.Server;
+
 public class App {
+    public static Server s;
 
     public static void main(String[] args) {
+        kaynnistaTietokanta();
+        
         KonsoliIO io = new KonsoliIO();
         Viitekirjanpito vkp = new Viitekirjanpito();
         Viitepalvelu vp = new Viitepalvelu(vkp);
         TekstiUI ui = new TekstiUI(io, vp);
         
         ui.run();
+        
+        sammutaTietokanta();
+    }
+
+    private static void kaynnistaTietokanta() {
+        System.out.println("Käynnistetään tietokanta...");
+        s = new Server();
+        s.setLogWriter(null);
+        s.setAddress("localhost");
+        s.setDatabaseName(0, "viitteet");
+        s.setDatabasePath(0, "file:tietokanta/viitteet");
+        s.start();
+    }
+
+    private static void sammutaTietokanta() {
+        System.out.println("Sammutetaan tietokanta...");
+        s.shutdown();
     }
 }
