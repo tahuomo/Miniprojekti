d1efb560344db15b102e85d8cbbfa6ba3a5e5c47
Eeva Terkki <juuuro@gmail.com>
2012-04-11 17:15:41 +0300

    Viitepalvelun junit-testeja

--- Viitejarjestelma/src/test/java/kumiankka/viitejarjestelma/ViitePalveluTest.java
+++ Viitejarjestelma/src/test/java/kumiankka/viitejarjestelma/ViitePalveluTest.java
@@ -1,5 +1,6 @@
 package kumiankka.viitejarjestelma;
 
+import java.util.List;
 import org.junit.*;
 import static org.junit.Assert.*;
 
@@ -23,12 +24,13 @@ public class ViitePalveluTest {
     public void setUp() {
         this.tallennusStub = new Viitekirjanpito();
         this.viitepalvelu = new Viitepalvelu(this.tallennusStub);
-        
+
         this.viitepalvelu.teeViite("artikkeli");
         this.viitepalvelu.lisaaViitteenYleisetTiedot("Otsikko", 2012);
-        this.viite = this.viitepalvelu.getViite();
-                
+        this.viitepalvelu.lisaaArtikkelinTiedot("Matin sanomat", 1, 2, 12, "ACM");
+        this.viitepalvelu.lisaaKirjoittaja("Matti", "Luukkainen");
 
+        this.viite = this.viitepalvelu.getViite();
     }
 
     @After
@@ -39,24 +41,76 @@ public class ViitePalveluTest {
     public void konstruktoriLuoOlion() {
         assertNotNull(this.viitepalvelu);
     }
-    
+
     @Test
     public void teeViiteLuoOlion() {
         assertNotNull(this.viitepalvelu.getViite());
     }
-    
+
     @Test
     public void teeViiteLuoOikeantyyppisenViitteen() {
         assertEquals(this.viite.getTyyppi(), "artikkeli");
     }
-    
-    @Test 
+
+    @Test
     public void viitteenOtsikkoLisataanOikein() {
         assertEquals(this.viite.getOtsikko(), "Otsikko");
     }
-    
+
     @Test
     public void viitteenVuosiLisataanOikein() {
         assertTrue(this.viite.getVuosi() == 2012);
+    }
+
+    @Test
+    public void artikkelinLehtiOikein() {
+        assertEquals(this.viite.getLehdenNimi(), "Matin sanomat");
+    }
+
+    @Test
+    public void artikkelinLehdenNumeroOikein() {
+        assertTrue(this.viite.getLehdenNumero() == 1);
+    }
+
+    @Test
+    public void artikkelinAlkusivuOikein() {
+        assertTrue(this.viite.getAloitusSivu() == 2);
+    }
+
+    @Test
+    public void artikkelinVikaSivuOikein() {
+        assertTrue(this.viite.getVikaSivu() == 12);
+    }
+
+    @Test
+    public void artikkelinJulkaisijaOikein() {
+        assertEquals(this.viite.getJulkaisija(), "ACM");
+    }
+
+    @Test
+    public void kirjoittajaLisataanOikein() {
+        List<Kirjoittaja> k = this.viite.getKirjoittajat();
+
+        assertTrue(k.size() == 1
+                && k.get(0).toString().equals("M. Luukkainen"));
+    }
+
+    @Test
+    public void viitteenTallennusToimii() {
+        this.viitepalvelu.tallennaViite();
+
+        assertEquals(this.viite, this.tallennusStub.viitteet.get(0));
+    }
+
+    @Test
+    public void olemattomienViitteidenListausToimii() {
+        assertEquals(this.viitepalvelu.listaaViitteet(), "");
+    }
+
+    @Test
+    public void viitteidenListausToimii() {
+        this.viitepalvelu.tallennaViite();
+
+        assertEquals(this.viitepalvelu.listaaViitteet(), "0 " + this.viite.toString() + "\n");
     }
 }
