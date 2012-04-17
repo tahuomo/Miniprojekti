7c44e2373777945987f011e3d4cb9959fef4ce5a
Tero Huomo <tero.huomo@helsinki.fi>
2012-04-17 18:56:02 +0300

    Rajapinnan päivitys

--- Viitejarjestelma/src/main/java/kumiankka/viitejarjestelma/IO.java
+++ Viitejarjestelma/src/main/java/kumiankka/viitejarjestelma/IO.java
@@ -2,6 +2,7 @@ package kumiankka.viitejarjestelma;
 
 public interface IO {
     void tulosta(String toPrint);
-    int lueLuku(String prompt);
-    String lueRivi(String prompt);    
+    int lueLuku(String prompt, boolean pakollinen);
+    String lueSyote(String prompt, boolean pakollinen);
+    public String LueRivi(String prompt);
 }
