ecaf6eb01411fe45c6d6b79c6f51d060e50eb76e
Tero Huomo <tero.huomo@helsinki.fi>
2012-04-17 18:57:57 +0300

    TekstiUIn uusi lisays

--- Viitejarjestelma/src/main/java/kumiankka/viitejarjestelma/TekstiUI.java
+++ Viitejarjestelma/src/main/java/kumiankka/viitejarjestelma/TekstiUI.java
@@ -13,7 +13,7 @@ public class TekstiUI {
         io.tulosta("Viitekirjanpito Hieno Ohjelma versio J.34H");
         io.tulosta("Kirjoita auta näyttääksesi komennot");
         while (true) {
-            String komento = io.lueRivi(">");
+            String komento = io.LueRivi(">");
 
             if (komento.equals("lopeta")) {
                 io.tulosta("Ohjelma lopetetaan.");
@@ -30,27 +30,73 @@ public class TekstiUI {
     }
 
     private void lisaaViite() {
-        String tyyppi = io.lueRivi("Kerro viitetyyppi:");
+        String tyyppi = kysyViitteenTyyppi();
         vp.teeViite(tyyppi);
-        kysyViitteenTiedot();
-        kysyArtikkelinTiedot();
+        kysyYleisetTiedot();      
+        if (tyyppi.equals("article")){
+            kysyArtikkelinTiedot();
+        } else if (tyyppi.equals("inproceedings")){
+            kysyKjulkaisunTiedot();
+        } else {
+            kysyKirjanTiedot();
+        }
         kysyKirjoittajat();
+        kysyTunniste();
         vp.tallennaViite();
         io.tulosta("Uusi viite lisättiin!");
     }
+    
+    private String kysyViitteenTyyppi(){
+        int valinta = -1;
+        while (valinta < 1 || valinta > 3) {
+            io.tulosta("Valitse viitteen tyyppi:\n");
+            io.tulosta("1. artikkeli\n2. konferenssijulkaisu\n3. kirja");
+            valinta = io.lueLuku("Valinta:", true);
+        }
+        if (valinta == 1){
+            return "article";
+        } else if (valinta == 2){
+            return "inproceedings";
+        } else {
+            return "book";
+        }
+    }
 
-    private void kysyViitteenTiedot() {
-        String otsikko = io.lueRivi("Otsikko:");
-        int vuosi = io.lueLuku("Julkaisuvuosi:");
+    private void kysyYleisetTiedot() {
+        String otsikko = io.lueSyote("Otsikko:", true);
+        int vuosi = io.lueLuku("Julkaisuvuosi:", true);
         vp.lisaaViitteenYleisetTiedot(otsikko, vuosi);
+    }
+    
+    private void kysyKirjanTiedot() {
+        String julkaisija = io.lueSyote("Julkaisija:", true);
+        String osoite = io.lueSyote("Julkaisijan osoite:", false);
+        String painos = io.lueSyote("Painos:", false);
+        String sarja = io.lueSyote("Kirjasarja:", false);
+
+        vp.lisaaKirjanTiedot(julkaisija, osoite, painos, sarja);
+    }
+    
+    private void kysyKjulkaisunTiedot() {
+        String kirjanNimi = io.lueSyote("Kirjan nimi:", true);
+        String julkaisija = io.lueSyote("Julkaisija:", false);
+        String osoite = io.lueSyote("Julkaisijan osoite:", false);
+        String sarja = io.lueSyote("Kirjasarja:", false);
+        int aloitusSivu = io.lueLuku("Ensimmäinen sivu:", false);
+        int lopetusSivu = io.lueLuku("Viimeinen sivu:", false);
+        String organisaatio = io.lueSyote("Organisaatio:", false);
+
+        vp.lisaaKonferenssijulkaisunTiedot(kirjanNimi, julkaisija
+                                           , osoite, sarja,
+                                           aloitusSivu, lopetusSivu, organisaatio);
     }
 
     private void kysyArtikkelinTiedot() {
-        String lehdenNimi = io.lueRivi("Lehden nimi:");
-        int lehdenNumero = io.lueLuku("Lehden numero:");
-        int aloitusSivu = io.lueLuku("Ensimmäinen sivu:");
-        int lopetusSivu = io.lueLuku("Viimeinen sivu:");
-        String julkaisija = io.lueRivi("Julkaisija:");
+        String lehdenNimi = io.lueSyote("Lehden nimi:", true);
+        int lehdenNumero = io.lueLuku("Lehden numero:", false);
+        int aloitusSivu = io.lueLuku("Ensimmäinen sivu:", false);
+        int lopetusSivu = io.lueLuku("Viimeinen sivu:", false);
+        String julkaisija = io.lueSyote("Julkaisija:", false);
 
         vp.lisaaArtikkelinTiedot(lehdenNimi, lehdenNumero, aloitusSivu, aloitusSivu, julkaisija);
     }
@@ -58,12 +104,12 @@ public class TekstiUI {
     private void kysyKirjoittajat() {
         io.tulosta("Anna kirjoittajat, tai anna tyhjä lopettaaksesi");
         while (true) {
-            String etunimi = io.lueRivi("Etunimi:");
-            if (etunimi.isEmpty()) {
+            String etunimi = io.lueSyote("Etunimi:", false);
+            if (etunimi == null) {
                 break;
             }
-            String sukunimi = io.lueRivi("Sukunimi:");
-            if (sukunimi.isEmpty()) {
+            String sukunimi = io.lueSyote("Sukunimi:", false);
+            if (sukunimi == null) {
                 break;
             }
             vp.lisaaKirjoittaja(etunimi, sukunimi);
@@ -87,5 +133,20 @@ public class TekstiUI {
         else {
             io.tulosta(viitteet);
         }
+    }
+
+    private void kysyTunniste() {
+        String tunniste = vp.generoiTunniste();
+        io.tulosta("Tunnisteeksi generoitiin " + tunniste);
+        while (true){
+            String syote = io.lueSyote("Syötä oma tai anna tyhjä:",false);
+            if (syote == null) break;
+            if (vp.tunnisteKelpaa(syote)){
+                tunniste = syote;
+                break;
+            }
+            io.tulosta("Tunniste ei uniikki.");
+        }
+        vp.lisaaViitteenTunniste(tunniste);
     }
 }
