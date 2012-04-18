19594c2a1ae48405c085a020a1c7fa7bedeb7bbd
Eeva Terkki <juuuro@gmail.com>
2012-04-18 19:57:05 +0300

    skandien korvaajan junit-testit

---
+++ Viitejarjestelma/src/test/java/kumiankka/viitejarjestelma/SkandienKorvaajaTest.java
@@ -0,0 +1,50 @@
+package kumiankka.viitejarjestelma;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SkandienKorvaajaTest {
+    public SkandienKorvaajaTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    
+    @Test
+    public void pikkuAOikein() {
+        assertEquals(SkandienKorvaaja.skanditBibtexYstavallisiksi("Mä"), "M\\\"{a}");
+    }
+    
+    @Test
+    public void isoAOikein() {
+        assertEquals(SkandienKorvaaja.skanditBibtexYstavallisiksi("MÄ"), "M\\\"{A}");
+    }
+    
+    @Test
+    public void pikkuOOikein() {
+        assertEquals(SkandienKorvaaja.skanditBibtexYstavallisiksi("Mö"), "M\\\"{o}");
+    }
+    
+    @Test
+    public void isoOOikein() {
+        assertEquals(SkandienKorvaaja.skanditBibtexYstavallisiksi("MÖ"), "M\\\"{O}");
+    }
+}
